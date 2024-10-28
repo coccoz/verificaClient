b895da299a9a8d9808ab1ad34ed6ddaa16521678
cosimo ballerini <ballerini.co@gmail.com>
2024-10-28 11:52:30 +0100

    esercizio base

--- verificaclient/src/main/java/com/example/Main.java
+++ verificaclient/src/main/java/com/example/Main.java
@@ -1,7 +1,47 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnknownHostException, IOException 
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Il Client è stato avviato");
+        
+        Socket s = new Socket("localhost",3000);
+
+        System.out.println("Il client si è collegato");
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+        String message;
+        do 
+        {
+            message = sc.nextLine();
+            
+            out.writeBytes( message + "\n"); //messaggio da mandare al server
+
+            String stringaRicevuta = in.readLine(); //stringa ricevuta dal server
+
+            //if (message.equals("!") ) 
+            //{
+            //    break;
+            //}
+
+            System.out.println("La stringa restituita è: " + stringaRicevuta);
+
+            
+        } while (!message.equals("!") );
+        s.close();
+        
     }
 }
