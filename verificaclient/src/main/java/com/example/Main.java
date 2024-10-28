d5dc82a161687e02728993f49cf1c3041b19db76
cosimo ballerini <ballerini.co@gmail.com>
2024-10-28 13:26:42 +0100

    tutto finito e funzionante

--- verificaclient/src/main/java/com/example/Main.java
+++ verificaclient/src/main/java/com/example/Main.java
@@ -15,33 +15,62 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Il Client è stato avviato");
-        
+            
         Socket s = new Socket("localhost",3000);
 
         System.out.println("Il client si è collegato");
-        
-        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
-        String message;
         do 
         {
-            message = sc.nextLine();
             
-            out.writeBytes( message + "\n"); //messaggio da mandare al server
-
-            String stringaRicevuta = in.readLine(); //stringa ricevuta dal server
-
-            //if (message.equals("!") ) 
-            //{
-            //    break;
-            //}
-
-            System.out.println("La stringa restituita è: " + stringaRicevuta);
-
             
-        } while (!message.equals("!") );
-        s.close();
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+            String message;
+            do 
+            {
+                message = sc.nextLine();
+                
+                if(message!=null)
+                {
+                    out.writeBytes( message + "\n"); //messaggio da mandare al server
+                }
+            
+                
+
+                String stringaRicevuta = in.readLine(); //stringa ricevuta dal server
+
+                if (stringaRicevuta.equals("!") ) 
+                {
+                    System.out.println("Connessione terminata");
+                    break;
+                }
+
+                else if(message.equals("?"))
+                {
+                    while(!(stringaRicevuta.equals("@")))
+                    {
+                        System.out.println("La stringa restituita è: " + stringaRicevuta);
+
+                        stringaRicevuta = in.readLine(); //stringa ricevuta dal server
+
+                        if(stringaRicevuta.equals("@"))
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    System.out.println("La stringa restituita è: " + stringaRicevuta);
+                }
+
+                
+            } while (!message.equals("!") );
+            s.close();
+            sc.close();
+        }while (true);
         
     }
 }
